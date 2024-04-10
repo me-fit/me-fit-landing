c6679a758edd8ed7f9168b84e7f2f3c1341616d2
realappie <abdelelmedny@gmail.com>
2024-04-10 13:16:03 +0900

    feat(app): improve app screen

--- src/app/[locale]/app/page.tsx
+++ src/app/[locale]/app/page.tsx
@@ -5,6 +5,7 @@ import AppBenefits from "@/components/AppBenefits/AppBenefits";
 import Flex from "@/components/Flex/Flex";
 import Image from "next/image";
 import TestimonialsList from "@/components/TestimonialsList/TestimonialsList";
+import StoreButtons from "@/components/StoreButtons/StoreButtons";
 type PageProps = {
   params: { locale: Locale };
 };
@@ -13,9 +14,33 @@ export default function Page({ params: { locale } }: PageProps) {
   const { formatMessage } = getIntl(locale);
   return (
     <main>
-      <section className={`${styles.padding} light-background`}>
-        <h1>{formatMessage({ id: "mefit.app.page.title" })}</h1>
-        <p>{formatMessage({ id: "mefit.app.page.paragraph" })}</p>
+      <section className={`${styles.firstSection} light-background`}>
+        <Flex
+          gap="1rem"
+          rowGap="4rem"
+          flexWrap="wrap"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <div className={styles.firstSectionLeftBlock}>
+            <h4>{formatMessage({ id: "mefit.app.page.header.one" })}</h4>
+            <h2>{formatMessage({ id: "mefit.app.page.header.two" })}</h2>
+            <p>{formatMessage({ id: "mefit.app.page.paragraph" })}</p>
+
+            <StoreButtons locale={locale} />
+          </div>
+
+          <Image
+            className={styles.appMainScreenshot}
+            priority={true}
+            src="/img/me-fit-app-2-devices-screenshots.png"
+            alt={formatMessage({
+              id: "mefit.app.page.photo.alt.text",
+            })}
+            width={1743}
+            height={2076}
+          />
+        </Flex>
       </section>
 
       <section className={styles.benefits}>
