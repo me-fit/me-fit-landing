d4eb6fb0a3b61763c7ce704d27730ed392f8965f
Eyadsaeed66 <saeedeyad713@gmail.com>
2024-12-02 17:47:03 +0200

    Adding images to some pages

--- src/app/[locale]/physiotherapy/page.tsx
+++ src/app/[locale]/physiotherapy/page.tsx
@@ -2,6 +2,7 @@ import { Locale } from "@/lib/intl";
 import { getIntl } from "@/lib/intl";
 import Link from "next/link";
 import Flex from "@/components/Flex/Flex";
+import Image from "next/image";
 import ArticlePage from "@/components/ArticlePage/ArticlePage";
 
 type PageProps = {
@@ -16,12 +17,44 @@ export default function Page({ params: { locale } }: PageProps) {
       <h5>{formatMessage({ id: "physiotherapy.page.sub.header" })}</h5>
       <h2>{formatMessage({ id: "physiotherapy.page.header.one" })}</h2>
       <p>{formatMessage({ id: "physiotherapy.page.paragraph.one" })}</p>
+      <Flex justifyContent="center">
+      <Image 
+       src="/img/me-fit-pro-exercise-libraryy.png"
+       alt=""
+       width={1024}
+       height={400}
+      />
+      </Flex>
       <h2>{formatMessage({ id: "physiotherapy.page.header.two" })}</h2>
       <p>{formatMessage({ id: "physiotherapy.page.paragraph.two" })}</p>
+      <Flex justifyContent="center">
+      <Image 
+       src="/img/Workout1.png"
+       alt=""
+       width={1024}
+       height={400}
+      />
+      </Flex>
       <h2>{formatMessage({ id: "physiotherapy.page.header.three" })}</h2>
       <p>{formatMessage({ id: "physiotherapy.page.paragraph.three" })}</p>
+      <Flex justifyContent="center">
+      <Image 
+       src="/img/Calendarr.png"
+       alt=""
+       width={1024}
+       height={400}
+      />
+      </Flex>
       <h2>{formatMessage({ id: "physiotherapy.page.header.four" })}</h2>
       <p>{formatMessage({ id: "physiotherapy.page.paragraph.four" })}</p>
+      <Flex justifyContent="center">
+        <Image
+        src="/img/me-fit-pro-workout-details-front.png"
+        alt=""
+        width={1028}
+        height={400}
+        />
+       </Flex>
 
       <br />
 
