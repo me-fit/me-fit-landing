dea5c124d31be45df60d1bddeb9b766e056e035a
Abdel <abdelelmedny@gmail.com>
2026-08-05 21:14:57 +0200

    Refactor(Venlo): remove customer

--- src/components/CustomersList/CustomersList.tsx
+++ src/components/CustomersList/CustomersList.tsx
@@ -28,12 +28,6 @@ const customers = [
     name: "PEC Zwolle",
     link: "https://peczwolle.nl/",
   },
-    {
-       imgSrc: "/img/customers/vvv-venlo.svg.png",
-    name: "VVV-Venlo",
-    link: "https://www.vvv-venlo.nl/",
-    }
-  
 ] as const;
 
 function CustomersList({ locale, className }: CustomersListProps) {
